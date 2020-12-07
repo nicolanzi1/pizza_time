ba1065306ae4415e9ea36f0cf15a7a9ba2717407
nicolanzi1 <nicolanzi@gmail.com>
2020-12-07 16:28:30 +0000

    Built the Navbar component and wrote its different elements whilst styling them with styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pizza Time</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   )
 }
 
